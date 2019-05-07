46cd4a30fafebf5a3ac9f2960770273384279430
Barbycris <barbara.pena.a@usach.cl>
2019-05-07 19:32:08 -0400

    file y user incompleto

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -1,10 +1,53 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\file;
 use Illuminate\Http\Request;
 
 class FileController extends Controller
 {
-    //
+   public function index() {
+   	$file = file::all();
+   	
+   	return $file;
+
+   }
+
+   public function show($id) { 
+   	$file= file::find($id);
+
+   	return $file;
+
+   }
+
+
+   public function create() {
+   	
+   }
+
+   public function store(Request $request) {
+
+   file::create($request->all());
+   
+   }
+
+   public function edit($id) {
+
+   $file= file::find($id);
+
+   return $file;
+
+   }
+
+   public function update(Request $request, $id) {
+
+   	$file= file::find($id);
+   	$file->update($request->all());
+
+   }
+
+   public function destroy($id) {
+
+   	//la consideramos? 
+   }
 }
--- app/categorie.php
+++ app/categorie.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Catogory extends Model
+class Categorie extends Model
 {
     protected $fillable = [
         'category_name',
