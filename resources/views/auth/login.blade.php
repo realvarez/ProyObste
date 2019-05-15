3f74e5bdcfc61473f0983851c14749315f1d961a
Ricardo <ricardo.alvarezz@usach.cl>
2019-05-15 16:13:27 -0400

    cambio login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,8 +2,7 @@
     @section('content')
     <div class="container">
         <div class="row justify-content-center">
-                {{ csrf_field() }}
-                <login-component></login-component>
+            <login-component></login-component>
         </div>
     </div>
 @endsection
