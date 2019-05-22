cd9c87e4d3de884b4ab9bab7315702b98acc13b6
Barbara <barbara.pena.a@usach.cl>
2019-05-21 23:12:18 -0400

    arreglo home

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -48,18 +48,18 @@
      
 
         <a href="category/{{$category->id}}" class="col-xs-12 col-md-6 col-lg-6 col-xl-3">
-            <div class="card-box noradius noborder bg-info">
+            <div class="card-box noradius noborder bg-info" style=" box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);">
                 
-                <h6 class="text-white text-uppercase m-b-20 text-center">
+                <h6 class="text-white text-uppercase m-b-20 text-center" style="text-shadow: 1px 1px 6px #185b6b;">
                 <i class="fas fa-folder" style="font-size: 20px;"></i>&nbsp&nbsp{{ucfirst($category->category_name)}}</h6>
               
             </div>
         </a>
         @endforeach
         <a href="/new-category" class="col-xs-12 col-md-6 col-lg-6 col-xl-3">
-        <div class="card-box noradius noborder bg-warning">
+        <div class="card-box noradius noborder bg-warning" style=" box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);">
                 
-                <h6 class="text-white text-uppercase m-b-20 text-center">
+                <h6 class="text-white text-uppercase m-b-20 text-center" style="text-shadow: 1px 1px 6px #a87e2a;">
                 <i class="fas fa-folder-plus" style="font-size: 20px;"></i>&nbsp&nbsp Nueva Categoría</h6>
             </div>
         </a>
