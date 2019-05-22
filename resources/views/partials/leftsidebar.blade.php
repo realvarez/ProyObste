f47e527efb95dd3c59a5f5eeaf036c6c444c17b0
Ricardo <ricardo.alvarezz@usach.cl>
2019-05-22 15:40:14 -0400

    fix2

--- resources/views/partials/leftsidebar.blade.php
+++ resources/views/partials/leftsidebar.blade.php
@@ -1,33 +1,34 @@
 
 <div class="left main-sidebar">
-	<div class="sidebar-inner leftscroll">
-		<div id="sidebar-menu">
-			<ul>
-
-				@foreach ($categories as $category)
-					@if(!isset($category->sons))
-						<li class="submenu">
-							<a href="{{route('category.subcategory.show', ['category'=>$category->id, 'subcategory'=> 'none'])}}"><i class="fa fa-fw fa-bars"></i><span>{{ucfirst($category->category_name)}}</span> </a>
-						</li>
-					@else
-						<li class="submenu">
-							<a><i class="fas fa-folder-open"></i><span>{{ucfirst($category->category_name)}}</span><span class="menu-arrow"></span></a>
-							<ul class="list-unstyled">
-								@foreach ($category->sons as $subCategory)
-									<li><a href="category/{{$subCategory->id}}">{{$subCategory->category_name}}</a></li>
-								@endforeach
-							</ul>
-						</li>
-					@endif
-				@endforeach
-				<li>
-					<a href="/category/create" id="subirarchivo" class="button">
-						<i class="fas fa-folder-plus"></i>Nueva categoría
-					</a>
-				</li>
-			</ul>
+		<div class="sidebar-inner leftscroll">
+			<div id="sidebar-menu">
+				<ul>
+					
+					@foreach ($categories as $category)
+						@if(!isset($category->sons))
+							<li class="submenu">
+								<a href="/category/{{$category->id}}"><i class="fa fa-fw fa-bars"></i><span>{{ucfirst($category->category_name)}}</span> </a>
+							</li>
+						@else
+							<li class="submenu">
+								<a><i class="fas fa-folder-open"></i><span>{{ucfirst($category->category_name)}}</span><span class="menu-arrow"></span></a>
+								<ul class="list-unstyled">
+									@foreach ($category->sons as $subCategory)
+										<li><a href="/category/{{$subCategory->id}}">{{$subCategory->category_name}}</a></li>
+									@endforeach
+								</ul>
+							</li>
+						@endif
+					@endforeach
+					<li>
+						<a href="/category/create" id="subirarchivo" class="button">
+							<i class="fas fa-folder-plus"></i>Nueva categoría
+						</a>
+					</li>
+				</ul>
+				<div class="clearfix"></div>
+			</div>
 			<div class="clearfix"></div>
 		</div>
-		<div class="clearfix"></div>
 	</div>
-</div>
+	
